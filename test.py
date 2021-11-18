aacaf45246a100aff4d77f1f95700a804d2f68e5
Roche Eliott <eliott.roche@epitech.eu>
2021-11-18 15:31:25 +0400

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+fruit = "pomme"
+quantity = 5
+pie_crust = "empty"
+isOvenOn = True
+
+print("You have", quantity, fruit,"and the pie crust is", pie_crust, "is the oven on ?", isOvenOn)
+
+
