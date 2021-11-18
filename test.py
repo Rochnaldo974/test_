aed1dc70fdd309eb43cd3fab8a1816d340d6c931
Roche Eliott <eliott.roche@epitech.eu>
2021-11-18 16:30:41 +0400

    task5

--- test.py
+++ test.py
@@ -9,8 +9,13 @@ def prep_my_fruit(quantity, fruit, pie_crust):
     print("You put", quantity, fruit, "on the trust")
     pie_crust = "filled with delicious apples"
     return (pie_crust)
+
+def turn_oven_on(isOvenOn):
+    isOvenOn = True
+    return (isOvenOn)
    
     
 
 pie_crust = prep_my_fruit(3, "apples", "empty")
 print("my pie is", pie_crust)
+print("Is the oven turned on ?", turn_oven_on(isOvenOn))
