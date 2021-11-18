ec027bb6e5257dcf5c332616b6551ad97779c469
Roche Eliott <eliott.roche@epitech.eu>
2021-11-18 15:34:59 +0400

    test2

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-fruit = "pomme"
-quantity = 5
-pie_crust = "empty"
-isOvenOn = True
+def print_hello_please(myName):
+    print ("hello", myName)
 
-print("You have", quantity, fruit,"and the pie crust is", pie_crust, "is the oven on ?", isOvenOn)
-
+print_hello_please("Jacqueline")
 
